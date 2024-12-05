3a1d577fa126d1467104bb3a904c7608c1658862
pcruzn <pablo.cruz@gmail.com>
2024-12-04 21:40:53 -0300

    Initial commit

---
+++ request_handler.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import patient_service
+
+app = FastAPI()
+
+@app.get("/patient/{patient_id}")
+def patient_details(patient_id: str):
+    result = patient_service.get_patient(patient_id)
+
+    return result
+
+
